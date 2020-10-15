fd4fbb46c1d73e1db2a732369cd7975cd7351394
roy cabouly <htr751@gmail.com>
2020-10-15 21:56:45 +0300

    Add Assests to game.
    The Assets contain images used as textures for the game.
    Changed AssetsDefinitions.h to contain the right paths to those Assets

--- src/include/AssetsDefinitions.h
+++ src/include/AssetsDefinitions.h
@@ -1,9 +1,9 @@
 #ifndef ASSETS_DEFINITIONS
 #define ASSETS_DEFINITIONS
 
-inline const char* treeWorldMapFilePath = "Assets/Graphics/bg.png";
-inline const char* heroFilePath = "Assets/Graphics/hero.png";
-inline const char* enemyFilePath = "Assets/Graphics/enemy.png";
-inline const char* rocketFilePath = "Assets/Graphics/rocket.png";
+inline const char* treeWorldMapFilePath = "../../Assets/Graphics/bg.png";
+inline const char* heroFilePath = "../../Assets/Graphics/hero.png";
+inline const char* enemyFilePath = "../../Assets/Graphics/enemy.png";
+inline const char* rocketFilePath = "../../Assets/Graphics/rocket.png";
 
 #endif
