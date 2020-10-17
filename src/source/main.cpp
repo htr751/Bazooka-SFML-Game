44e15620c44d8b5f7e4efac885079c8bfe4aa917
roy cabouly <htr751@gmail.com>
2020-10-17 07:42:50 -0700

    Change window label to game name

--- src/source/main.cpp
+++ src/source/main.cpp
@@ -6,7 +6,7 @@ int main()
 {
 	sf::Vector2f viewSize(1024, 768);
 	sf::VideoMode viewMode(viewSize.x, viewSize.y);
-	sf::RenderWindow window(viewMode, "First SFML game", sf::Style::Default);
+	sf::RenderWindow window(viewMode, "Bazooka", sf::Style::Default);
 	
 	Game game{ window };
 	while (game.start(2) == Game::GameOverStatus::RestartGame);
